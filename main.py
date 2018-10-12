99198e3c190a2720f0a26d9f267b95d7f1ed9755
Olga Sorokina <sorokinaog@icloud.com>
2018-10-12 00:32:13 -0400

    Implemented basic tasks with no css added

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://build-a-blog:qwerty@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO']=True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(255))
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route('/')
+def index():
+    return redirect('/blog')
+
+@app.route('/blog', methods =['GET'])
+def blog():
+    id = request.args.get('id')
+    if id:
+        blog = Blog.query.filter_by(id = id).first()
+        return render_template('singlepost.html', blog=blog)
+    else:    
+        blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs)
+
+@app.route('/newpost', methods =['POST','GET'])
+def newpost():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_blog = Blog(title,body)
+        
+        if title =='' and body =='':
+            error = 'empty entries'
+            return redirect('/newpost?error='+error)
+        if title =='':
+            error = 'empty title'
+            return redirect('/newpost?error='+error)
+        if body == '':
+            error = 'empty body'
+            return redirect('/newpost?error='+error)
+
+        db.session.add(new_blog)
+        db.session.commit()
+        id = new_blog.id
+        return redirect('/blog?id={0}'.format(id))
+    error = request.args.get('error')
+    return render_template('newpost.html', error = error) 
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
